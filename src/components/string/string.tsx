c57fba858f1ef0d807f8855a203040e5e2ab437d
Monotek <timur.yakhin.99@gmail.com>
2024-04-07 20:25:15 +0300

    push

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, ChangeEvent, MouseEvent } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
-
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import style from "./string.module.css";
 export const StringComponent: React.FC = () => {
+  const [value, setValue] = useState<string>('');
+  const setResult = (e: ChangeEvent<HTMLInputElement>)=>{
+      setValue(e.target.value);
+  }
+  const onClick = (e:MouseEvent<HTMLButtonElement>) => {
+    
+  }
+  
   return (
     <SolutionLayout title="Строка">
-     <></>
+      <div className={style.container}>
+        <form action="" className={style.form}>
+          <div className={style.column}>
+            <Input extraClass={style.input} maxLength={11} value={value} onChange={setResult}/>
+            <p className={style.pre}>Максимум — 11 символов</p>
+          </div>
+          <Button text="Развернуть" onClick={e => onClick(e)}/>
+        </form>
+        <div className="result">
+
+        </div>
+      </div>
     </SolutionLayout>
   );
 };
