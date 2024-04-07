d5a72c08a3afe3c64622b0d8413b5dab4780e0ab
Monotek <timur.yakhin.99@gmail.com>
2024-04-07 11:35:37 +0300

    add branch

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -4,7 +4,7 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+     <></>
     </SolutionLayout>
   );
 };
