bb06af0f09e92cdd55fbdd9116b272fc0b6adbda
Timur Yakhin <timur.yakhin.99@gmail.com>
2024-04-10 15:19:38 +0300

    fibonachi  done

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,61 +1,62 @@
 import React, { useState, ChangeEvent, FormEvent } from "react";
 import { Circle } from "../ui/circle/circle";
-import { ElementStates } from "../../types/element-states";
-import { DELAY_IN_MS } from "../../constants/delays";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
-import style from './fibonacci-page.module.css'
+import style from './fibonacci-page.module.css';
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 export const FibonacciPage: React.FC = () => {
-  type TResult = {
-    value: number;
-    state: ElementStates
-}
   const [isValid, setIsValid] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<string>('');
-  const [result, setResult] = useState<TResult[]>([]);
+  const [result, setResult] = useState<number[]>([]);
   const [isLoader, setIsLoader] = useState(false);
   const max = 19;
   function delay(time: number = 0) {
-      return new Promise(resolve => setTimeout(resolve, time));
+    return new Promise(resolve => setTimeout(resolve, time));
   }
 
   function handleInput(e: ChangeEvent<HTMLInputElement>) {
-      const curNumber = +e.target.value;
-      setInputValue(e.target.value);
-      if (curNumber > 0 && curNumber <= max) {
-          setIsValid(true);
-      } else {
-          setIsValid(false);
-      }
+    const curNumber = +e.target.value;
+    setInputValue(e.target.value);
+    if (curNumber > 0 && curNumber <= max) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
   }
 
   function handleSubmit(e: FormEvent) {
-      e.preventDefault();
-      fibonacci(+inputValue)
-      .then(result => setResult([{value: result, state:ElementStates.Changing}]))
-      .then(() => console.log(result))
+    e.preventDefault();
+    setResult([]);
+    fibonacci(+inputValue);
   }
-  async function fibonacci (n: number, memo: Record<number, number> = {}) {
-    if (n in memo) {
-      return memo[n];
+  async function fibonacci(n: number) {
+    let arr: number[] = [];
+    for(let i = 0; i < n+1; i++){
+     i && await delay(SHORT_DELAY_IN_MS);
+     i < 2 && arr.push(1);
+     i >= 2 && arr.push(arr[i-2] + arr[i-1]);
+      setResult([...arr]);
     }
-    if (n <= 2) {
-      return 1;
-    }
-    memo[n] = await fibonacci(n - 1, memo) + await fibonacci(n - 2, memo);
-    return memo[n]
-  }; 
+    return arr[n];
+  };
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
       <div className={style.container}>
         <form className={style.form} onSubmit={handleSubmit}>
           <div className={style.column}>
-            <Input type="number" extraClass={style.input} max={19} value={inputValue} onChange={handleInput} />
+            <Input type="number" extraClass={style.input} min={1} max={19} value={inputValue} onChange={handleInput} />
             <p className={style.pre}>Максимальное число — 19</p>
           </div>
           <Button type="submit" text="Рассчитать" disabled={!isValid} isLoader={isLoader} />
         </form>
+        <div className={style.result}>
+          {
+            result.map((item, index) => {
+              return <Circle key={index} letter={item.toString()} index={index} extraClass="mb-20"/>
+            })
+          }
+        </div>
       </div>
     </SolutionLayout>
   );
--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
-
+import { Button } from "../ui/button/button";
+import { RadioInput } from "../ui/radio-input/radio-input";
+import style from './sorting-page.module.css';
+import { Direction } from "../../types/direction";
 export const SortingPage: React.FC = () => {
+  const [isValid, setIsValid] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [result, setResult] = useState<number[]>([]);
+  const [isLoader, setIsLoader] = useState(false);
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    setResult([]);
+
+  }
   return (
     <SolutionLayout title="Сортировка массива">
+      <div className={style.container}>
+                <form className={style.form} onSubmit={handleSubmit}>
+                    <RadioInput label="Выбор"/>
+                    <RadioInput label="Пузырек"/>
+                    <Button type="button" text="По возрастанию" sorting={Direction.Ascending} isLoader={isLoader} />
+                    <Button type="button" text="По убыванию" sorting={Direction.Descending} isLoader={isLoader} />
+                    <Button type="button" text="Новый массив" isLoader={isLoader} />
 
+                </form>
+                <div className={style.result}>
+                    {
+
+                    }
+                </div>
+            </div>
     </SolutionLayout>
   );
 };
